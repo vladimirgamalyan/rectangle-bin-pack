2d35ea6cfdc534b523e94ff76d79555c4c04def5
Vladimir Gamalian <vladimir.gamalian@gmail.com>
2016-07-31 08:58:16 +0700

    Add parse and modify arguments

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const addon = require('./build/Release/rectangle_bin_pack');
 
-console.log(addon.solve());
+var a = [{
+	w: 12,
+	h: 42
+}, {
+	w: 2,
+	h: 34
+}, {
+	w: 22,
+	h: 24
+}];
+
+addon.solve(a);
+console.log(a);
--- main.cpp
+++ main.cpp
@@ -2,13 +2,59 @@
 
 using namespace v8;
 
-void Method(const FunctionCallbackInfo<Value>& args) {
+void Solve(const FunctionCallbackInfo<Value>& args) {
 	Isolate* isolate = args.GetIsolate();
-	args.GetReturnValue().Set(String::NewFromUtf8(isolate, "ok"));
+
+	if (args.Length() < 1) {
+		isolate->ThrowException(Exception::TypeError(
+			String::NewFromUtf8(isolate, "Wrong number of arguments")));
+		return;
+	}
+
+	if (!args[0]->IsArray()) {
+		isolate->ThrowException(Exception::TypeError(
+			String::NewFromUtf8(isolate, "Wrong arguments")));
+		return;
+	}
+	Local<Array> arr = Local<Array>::Cast(args[0]);
+	uint32_t len = arr->Length();
+
+	for (uint32_t i = 0; i < len; ++i) {
+		Local<Value> item = arr->Get(i);
+		if (!item->IsObject()) {
+			isolate->ThrowException(Exception::TypeError(
+				String::NewFromUtf8(isolate, "Wrong arguments")));
+			return;
+		}
+		Local<Object> obj = item->ToObject();
+		Local<Value> w = obj->Get(String::NewFromUtf8(isolate, "w"));
+		Local<Value> h = obj->Get(String::NewFromUtf8(isolate, "h"));
+
+		// 0..0xffffffff only
+		if ((!w->IsUint32()) || (!h->IsUint32())) {
+			isolate->ThrowException(Exception::TypeError(
+				String::NewFromUtf8(isolate, "Wrong arguments")));
+			return;
+		}
+		uint32_t width = w->Uint32Value();
+		uint32_t height = h->Uint32Value();
+
+		if ((!width) || (!height) || (width > 0xffff) || (height > 0xffff)) {
+			isolate->ThrowException(Exception::TypeError(
+				String::NewFromUtf8(isolate, "Wrong arguments")));
+			return;
+		}
+
+		uint32_t x = 0;
+		uint32_t y = 0;
+
+		obj->Set(String::NewFromUtf8(isolate, "x"), Uint32::New(isolate, x));
+		obj->Set(String::NewFromUtf8(isolate, "y"), Uint32::New(isolate, y));
+	}
 }
 
 void init(Local<Object> exports) {
-	NODE_SET_METHOD(exports, "solve", Method);
+	NODE_SET_METHOD(exports, "solve", Solve);
 }
 
 NODE_MODULE(rectangle_bin_pack, init)
