c793dd56d90d98d746bf0d23461de03b08ad3653
Vladimir Gamalian <vladimir.gamalian@gmail.com>
2016-08-04 08:03:43 +0700

    Improve basic test

--- test/test.js
+++ test/test.js
@@ -1,12 +1,13 @@
-var assert = require('assert');
+var expect = require('chai').expect;
 var rbp = require('../build/Release/rectangle_bin_pack');
 
 describe('rectangleBinPack', function () {
 	
 	it('basic call', function (done) {
-		rbp.solve([{w:64, h:64}], function (err) {
-			if (err)
-				throw err;
+		rbp.solve([{w:64, h:64}], function (err, res) {
+			expect(err).to.be.null;
+			expect(res).to.be.an('array').with.lengthOf(1);
+			expect(res).to.deep.equal([{x:0, y:0, i:0}]);
 			done();
 		});
 	});
