5a0658a7814276053d00c9cc0ea12289d030cede
Vladimir Gamalian <vladimir.gamalian@gmail.com>
2016-08-13 17:42:17 +0700

    Add tests

--- test/test.js
+++ test/test.js
@@ -30,14 +30,53 @@ describe('rectangleBinPack', function () {
 			expect(a).to.be.an('array').with.lengthOf(1);
 			expect(a).to.deep.equal([{x:0, y:0, w:64, h:64}]);
 		});
+		
+		it('two rectangles sync', function () {
+			var a = [{w:32, h:32}, {w:64, h:64}];
+			var result = rbp.solveSync({}, a);
+			expect(result).to.be.ok;
+			expect(a).to.be.an('array').with.lengthOf(2);
+			expect(a).to.deep.equal([{x:0, y:0, w:32, h:32}, {x:0, y:64, w:64, h:64}]);
+		});		
 	
 	});
 	
 	describe('exceptions', function() {
 		it('not enough arguments', function () {
+			expect(rbp.solve.bind(null)).to.throw('not enough arguments');
+			expect(rbp.solve.bind(null, {})).to.throw('not enough arguments');
+			expect(rbp.solve.bind(null, {}, [])).to.throw('not enough arguments');
+			expect(rbp.solve.bind(null, {}, [], null)).not.to.throw('not enough arguments');
+		});
+		
+
+		it('first argument is not an object', function () {
+			expect(rbp.solve.bind(null, true, [], null)).to.throw('first argument is not an object');
+			expect(rbp.solve.bind(null, 42, [], null)).to.throw('first argument is not an object');
+			expect(rbp.solve.bind(null, [], [], null)).to.throw('first argument is not an object');
+			expect(rbp.solve.bind(null, {}, [], null)).not.to.throw('first argument is not an object');
+		});
+
+		it('second argument is not an array', function () {
+			expect(rbp.solve.bind(null, {}, true, null)).to.throw('second argument is not an array');
+			expect(rbp.solve.bind(null, {}, 42, null)).to.throw('second argument is not an array');
+			expect(rbp.solve.bind(null, {}, {}, null)).to.throw('second argument is not an array');
+			expect(rbp.solve.bind(null, {}, [], null)).not.to.throw('second argument is not an array');
+		});
+		
+		it('third argument is not a function', function () {
+			expect(rbp.solve.bind(null, {}, [], null)).to.throw('third argument is not a function');
+			expect(rbp.solve.bind(null, {}, [], true)).to.throw('third argument is not a function');
+			expect(rbp.solve.bind(null, {}, [], 42)).to.throw('third argument is not a function');
+			expect(rbp.solve.bind(null, {}, [], [])).to.throw('third argument is not a function');
+		});		
+	});
+	
+	describe('exceptions sync', function() {
+		it('not enough arguments', function () {
 			expect(rbp.solveSync.bind(null)).to.throw('not enough arguments');
 			expect(rbp.solveSync.bind(null, {})).to.throw('not enough arguments');
-			expect(rbp.solveSync.bind(null, {}, [])).not.to.throw('not enough arguments');
+			expect(rbp.solveSync.bind(null, {}, [])).not.throw('not enough arguments');
 		});
 		
 		it('first argument is not an object', function () {
@@ -53,7 +92,5 @@ describe('rectangleBinPack', function () {
 			expect(rbp.solveSync.bind(null, {}, {})).to.throw('second argument is not an array');
 			expect(rbp.solveSync.bind(null, {}, [])).not.to.throw('second argument is not an array');
 		});
-		
-		//TODO: async version
-	});
+	});	
 });
